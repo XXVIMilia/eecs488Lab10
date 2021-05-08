de11d113c31313fa552efa3dca9affc7f3895fa1
Chauncey Hester <c112h406@ku.edu>
2021-05-08 16:07:29 -0500

    Exercise1 test

--- Exercise2/CreateUser.php
+++ Exercise2/CreateUser.php
@@ -7,7 +7,7 @@ $mysqli = new mysqli("mysql.eecs.ku.edu", "chaunceyhester", "eithae7u", "chaunce
 function checkUserExist(){
     if($mysqli->connect_errno){
         printf("No Connect: %s\n",$mysqli->connect_errno);
-        exit();
+        //exit();
     }
     else{
         $query = "select user_id from user_id where exists (Select user_id from user_id where user_id = $user)";
