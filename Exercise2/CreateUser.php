e3a3624a4524e2400f5636fa956ba272b73c3b87
Chauncey Hester <c112h406@ku.edu>
2021-05-08 14:50:05 -0500

    Exercise1 test

--- Exercise2/CreateUser.php
+++ Exercise2/CreateUser.php
@@ -7,7 +7,7 @@ $mysqli = new mysqli("mysql.eecs.ku.edu", "chaunceyhester", "eithae7u", "chaunce
 function checkUserExist(){
     if($mysqli->connect_errno){
         printf("No Connect: %s\n",$mysqli->connect_errno);
-        exit();
+        return(false);
     }
     else{
         $query = "select user_id from users where exists (Select user_id from users where user_id == $user)";
@@ -27,27 +27,23 @@ echo "<html>";
 echo "<head>";
 echo "</head>";
 
-// if($user != ""){
-//     if(checkUserExist()){
-//         echo "<h1> Thank You for creating an account, $user</h1>";
-//         $query = "Insert into users values ($users)";
-//         if($result = $mysqli->query($query)){
-//             echo "<h1> Account creation successful</h1>";
-//         }
-//         else{
-//             echo "<h1> Account creation unsuccessful</h1>";
-//         }
+if($user != ""){
+    if(checkUserExist()){
+        echo "<h1> Thank You for creating an account, $user</h1>";
+        $query = "insert into users values ($users)";
+        if($result = $mysqli->query($query)){
+            echo "<h1> Account creation successful</h1>";
+        }
+        else{
+            echo "<h1> Account creation unsuccessful</h1>";
+        }
 
-//     }
-//     else{
-//         echo "<h1> The following username is taken: $user</h1>";
-//     }
+    }
+    else{
+        echo "<h1> The following username is taken: $user</h1>";
+    }
 
-// }
-
-echo "<h1> The following username is taken: $user</h1>";
-
-
+}
 
 echo "</html>";
 
