718370886f0a79c998bf4ecb389207e9cc84e8d2
Chauncey Hester <c112h406@ku.edu>
2021-05-09 12:16:14 -0500

    Admin and View users

--- Exercise5/ViewUsers.php
+++ Exercise5/ViewUsers.php
@@ -6,24 +6,25 @@ echo "<html>";
 echo "<head>";
 echo "</head>";
 
-$mysqli = new mysqli("mysql.eecs.ku.edu", "chaunceyhester", "eithae7u", "chaunceyhester");
-$query = "select user_id from users";
-        if($result = $mysqli->query($query)){
-            if($result->num_rows > 0){
-                echo $result['user_id'];
-            }
-            else{
-                echo "No users found";
-            }
-        }
-        else{
-            $mysqli->close();
-            printf("Command Failed");
-            die("Connection failed: " . $mysqli->connect_error);
-        }
+// $mysqli = new mysqli("mysql.eecs.ku.edu", "chaunceyhester", "eithae7u", "chaunceyhester");
+// $query = "select user_id from users";
+//         if($result = $mysqli->query($query)){
+//             if($result->num_rows > 0){
+//                 echo $result['user_id'];
+//             }
+//             else{
+//                 echo "No users found";
+//             }
+//         }
+//         else{
+//             $mysqli->close();
+//             printf("Command Failed");
+//             die("Connection failed: " . $mysqli->connect_error);
+//         }
 
 
-$mysqli->close();
+// $mysqli->close();
+echo "Fuck you"
 
 echo "</html>";
 
