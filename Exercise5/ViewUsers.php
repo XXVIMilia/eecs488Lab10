71b97dd7b001591bbec84bf80505dd71c20d5d51
Chauncey Hester <c112h406@ku.edu>
2021-05-09 12:13:19 -0500

    Admin and View users

--- Exercise5/ViewUsers.php
+++ Exercise5/ViewUsers.php
@@ -6,7 +6,7 @@ echo "<html>";
 echo "<head>";
 echo "</head>";
 
-
+$mysqli = new mysqli("mysql.eecs.ku.edu", "chaunceyhester", "eithae7u", "chaunceyhester");
 $query = "select user_id from users";
         if($result = $mysqli->query($query)){
             if($result->num_rows > 0){
