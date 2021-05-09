5967fe77f4967e367e355378eb98cb751eb462cb
Chauncey Hester <c112h406@ku.edu>
2021-05-09 15:18:36 -0500

    Delete Post pt 2

--- Exercise7/DeletePost.php
+++ Exercise7/DeletePost.php
@@ -9,21 +9,25 @@ echo "</head>";
 $post_id = $_POST["post_id"];
 $mysqli = new mysqli("mysql.eecs.ku.edu", "chaunceyhester", "eithae7u", "chaunceyhester");
 $query = "select user_id from users";
-        if($result = $mysqli->query($query)){
-            if($result->num_rows > 0){
-                while ($row = $result->fetch_assoc()) {
-                    echo $row["user_id"] . "<br>";
-                }
-            }
-            else{
-                echo "No users found";
-            }
-        }
-        else{
-            $mysqli->close();
-            printf("Command Failed");
-            die("Connection failed: " . $mysqli->connect_error);
-        }
+        // if($result = $mysqli->query($query)){
+        //     if($result->num_rows > 0){
+        //         while ($row = $result->fetch_assoc()) {
+        //             echo $row["user_id"] . "<br>";
+        //         }
+        //     }
+        //     else{
+        //         echo "No users found";
+        //     }
+        // }
+        // else{
+        //     $mysqli->close();
+        //     printf("Command Failed");
+        //     die("Connection failed: " . $mysqli->connect_error);
+        // }
+
+for($i = 0; $i < count($post_id); $i++){
+    echo $post[$i] . "<br>";
+}
 
 
 $mysqli->close();
