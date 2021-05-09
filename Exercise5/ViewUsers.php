2b796015f9f9b0f9f7af1d3d2985090a37c806ae
Chauncey Hester <c112h406@ku.edu>
2021-05-09 12:30:30 -0500

    Admin and View users

--- Exercise5/ViewUsers.php
+++ Exercise5/ViewUsers.php
@@ -11,7 +11,7 @@ $query = "select user_id from users";
         if($result = $mysqli->query($query)){
             if($result->num_rows > 0){
                 while ($row = $result->fetch_assoc()) {
-                    printf ("%s\n", $row["user_id"]);
+                    echo $row["user_id"];
                 }
             }
             else{
